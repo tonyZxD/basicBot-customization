7e11777f7c12c5b1071a8c6ae4a8ea8f85cd6afb
tonyZxD <antonijoivanic@net.hr>
2015-05-13 12:37:30 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -58,7 +58,7 @@
         botName: "basicBot",
         language: "english",
         startupCap: 1, // 1-200
-        startupVolume: 0, // 0-100
+        startupVolume: 80, // 0-100
         startupEmoji: false, // true or false
         cmdDeletion: true,
         chatLink: "https://rawgit.com/Yemasthui/basicBot/master/lang/en.json",
